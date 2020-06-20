9d9a0c439e55638f73a42ca7fe810964da00966a
AwradArrakAlanteri-77 <aw.ala@yahoo.com>
2020-06-20 23:39:11 +0300

    Class Work three is done.😆

---
+++ IOS-CW3-P1.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var number1 =  3
+var number2 = 4
+
+func add( a: Int, b: Int ) -> Int {
+    return a + b
+}
+
+var functionCall = add(a: number1, b: number2)
+print("\(number1) + \(number2) = \(functionCall)")
